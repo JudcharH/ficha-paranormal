8e7aa28be685dc151cb64781cd3cbedec02af296
JudcharH <Jakeuilsom245@gmail.com>
2026-05-27 02:25:12 -0300

    Atualização do sistema

--- inventory.js
+++ inventory.js
@@ -9,7 +9,7 @@ const itens = [
         descricao: "1d4 + FOR",
         ep: 1,
         categoria: "Corpo a Corpo",
-        usos: 0,
+        usos: 5,
 
         modo: "dano",
         dice: 1,
@@ -17,17 +17,383 @@ const itens = [
         bonusAttr: "forca"
     },
 
+     // ======================================
+    // CORPO A CORPO
+    // ======================================
+
+    {
+        nome: "Desarmado",
+        descricao: "1d4 + FOR",
+        ep: 0,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Adaga",
+        descricao: "1d6 + FOR",
+        ep: 1,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Taco",
+        descricao: "1d6 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Machete",
+        descricao: "1d6 + 1 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Lança",
+        descricao: "1d6 + 2 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Marreta",
+        descricao: "2d8 + 3 + FOR",
+        ep: 3,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Bastão",
+        descricao: "1d6 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Nunchaku",
+        descricao: "1d6 + 1 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Machadinha",
+        descricao: "1d6 + 2 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Machado",
+        descricao: "1d8 + 2 + FOR",
+        ep: 3,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Espada",
+        descricao: "1d10 + 2 + FOR",
+        ep: 3,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Maça",
+        descricao: "2d6 + 2 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Florete",
+        descricao: "1d6 + 2 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Katana",
+        descricao: "1d10 + 2 + FOR",
+        ep: 3,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Acha",
+        descricao: "1d12 + 2 + FOR",
+        ep: 3,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Gadanho",
+        descricao: "2d4 + 2 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Motosserra",
+        descricao: "3d6 + FOR",
+        ep: 3,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Montante",
+        descricao: "2d8 + 2 + FOR",
+        ep: 2,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Corrente",
+        descricao: "1d8 + 1 + FOR",
+        ep: 1,
+        categoria: "Corpo a Corpo",
+        usos: 5,
+        modo: "dano"
+    },
+
+     // ======================================
+    // ARCOS
+    // ======================================
+
+    {
+        nome: "Arco curto",
+        descricao: "1d6 + 2 + AGI",
+        ep: 2,
+        categoria: "Arco",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Arco composto",
+        descricao: "1d8 + 2 + AGI",
+        ep: 3,
+        categoria: "Arco",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Arco refinado",
+        descricao: "1d10 + 2 + AGI",
+        ep: 4,
+        categoria: "Arco",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Besta",
+        descricao: "1d6 + 3 + AGI",
+        ep: 2,
+        categoria: "Arco",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Besta refinada",
+        descricao: "1d8 + 3 + AGI",
+        ep: 3,
+        categoria: "Arco",
+        usos: 5,
+        modo: "dano"
+    },
+
+    // ======================================
+    // ARMAS DE FOGO
+    // ======================================
+
     {
         nome: "Pistola",
         descricao: "1d8 + AGI",
         ep: 2,
         categoria: "Arma de Fogo",
-        usos: 0,
+        usos: 5,
 
         modo: "dano",
         dice: 1,
         diceType: 8,
         bonusAttr: "agilidade"
+
+    },
+
+      {
+        nome: "Revólver",
+        descricao: "1d8 + 2 + AGI",
+        ep: 2,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Fuzil de caça",
+        descricao: "2d8 + AGI",
+        ep: 3,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Submetralhadora",
+        descricao: "2d6 + AGI",
+        ep: 3,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Espingarda",
+        descricao: "4d6 + AGI",
+        ep: 4,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Fuzil de assalto",
+        descricao: "2d8 + AGI",
+        ep: 4,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Fuzil de precisão",
+        descricao: "2d10 + 2 + AGI",
+        ep: 4,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Metralhadora",
+        descricao: "2d12 + 2 + AGI",
+        ep: 4,
+        categoria: "Arma de Fogo",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Lança-chamas",
+        descricao: "6d6 + AGI",
+        ep: 5,
+        categoria: "Arma Pesada",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Bazuca",
+        descricao: "10d8 + AGI",
+        ep: 5,
+        categoria: "Arma Pesada",
+        usos: 5,
+        modo: "dano"
+    },
+
+    // ======================================
+// EQUIPAMENTOS
+// ======================================
+
+
+    {
+        nome: "Proteção Leve",
+        descricao: "+5 Defesa",
+        ep: 2,
+        categoria: "Equipamento",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    {
+        nome: "Proteção Pesada",
+        descricao: "+10 Defesa",
+        ep: 4,
+        categoria: "Equipamento",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    {
+        nome: "Escudo",
+        descricao: "+2 Defesa",
+        ep: 2,
+        categoria: "Equipamento",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    {
+        nome: "Escudo Grande",
+        descricao: "+4 Defesa",
+        ep: 3,
+        categoria: "Equipamento",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+// ======================================
+// ITENS GERAIS
+// ======================================
+
+
+
+    {
+        nome: "Vestimenta",
+        descricao: "+2 em perícias",
+        ep: 1,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "equipamento"
     },
 
     {
@@ -41,9 +407,116 @@ const itens = [
         dice: 2,
         diceType: 8,
         bonus: 0
-    }
+    },
 
-];
+    {
+        nome: "Soqueira",
+        descricao: "+2 dano desarmado",
+        ep: 0.5,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    {
+        nome: "Kit de Reparos",
+        descricao: "Reparar itens",
+        ep: 2,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "suporte"
+    },
+
+    {
+        nome: "Cicatrizante",
+        descricao: "2d6 + 2",
+        ep: 1,
+        categoria: "Consumível",
+        usos: 5,
+        modo: "cura"
+    },
+
+    {
+        nome: "Binóculos",
+        descricao: "+5 Percepção",
+        ep: 1,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    {
+        nome: "Pé de Cabra",
+        descricao: "1d6 + FOR",
+        ep: 1,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "dano"
+    },
+
+    {
+        nome: "Mochila",
+        descricao: "+4 EP / -2 Furtividade",
+        ep: 0,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    {
+        nome: "Bolsa",
+        descricao: "+2 EP / -1 Furtividade",
+        ep: 0,
+        categoria: "Item Geral",
+        usos: 5,
+        modo: "equipamento"
+    },
+
+    // ======================================
+    // ALIMENTOS
+    // ======================================
+
+    {
+        nome: "Lanchinho",
+        descricao: "Recupera 3 PV e 1 PD",
+        ep: 0.5,
+        categoria: "Alimento",
+        usos: 5,
+        modo: "cura"
+    },
+
+    {
+        nome: "Lanche",
+        descricao: "Recupera 5 PV e 2 PD",
+        ep: 1,
+        categoria: "Alimento",
+        usos: 5,
+        modo: "cura"
+    },
+
+    {
+        nome: "Marmita",
+        descricao: "Recupera 7 PV e 3 PD",
+        ep: 2,
+        categoria: "Alimento",
+        usos: 5,
+        modo: "cura"
+    },
+
+    {
+        nome: "Banquete",
+        descricao: "Recupera 10 PV e 4 PD",
+        ep: 3,
+        categoria: "Alimento",
+        usos: 5,
+        modo: "cura"
+    },
+
+
+
+   
+
+]
 
 // ======================================
 // MODAL
